54214a7220f10f03b5a030029b7dc9412248623c
Andrii Chychkan <mywork1728@gmail>
2026-01-12 23:22:23 +0200

    added payment model

--- library/models.py
+++ library/models.py
@@ -11,7 +11,7 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     cover = models.CharField(
-        max_length=2,
+        max_length=4,
         choices=Cover,
     )
 
@@ -41,3 +41,31 @@ class Borrowing(models.Model):
 
     def __str__(self):
         return f"{self.user} borrowed {self.book}"
+
+
+class Payment(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "PN", _("Pending")
+        PAID = "PD", _("Paid")
+
+    class Type(models.TextChoices):
+        PAYMENT = "PM", _("Payment")
+        FINE = "FN", _("Fine")
+
+    status = models.CharField(
+        max_length=2,
+        choices=Status,
+        default=Status.PENDING
+    )
+    type = models.CharField(
+        max_length=2,
+        choices=Type
+    )
+    borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE,
+                                  related_name="payments")
+    session_url = models.URLField(max_length=500)
+    session_id = models.CharField(max_length=255, unique=True)
+    money = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+    )
