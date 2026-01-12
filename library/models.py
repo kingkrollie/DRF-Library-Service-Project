5f476e510868983fa1f469a72453934df059a2ba
Andrii Chychkan <mywork1728@gmail>
2026-01-12 23:04:37 +0200

    added borrowing model and added users and library apps into settings

--- library/models.py
+++ library/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -19,3 +20,24 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title}  {self.author}"
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name="borrowings"
+    )
+
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="borrowings"
+    )
+
+    def __str__(self):
+        return f"{self.user} borrowed {self.book}"
