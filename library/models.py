f0431e364485bf49735123c3e15db623df50f722
Andrii Chychkan <mywork1728@gmail>
2026-01-13 16:27:25 +0200

    created app payment and payment model moved inside it

--- library/models.py
+++ library/models.py
@@ -41,31 +41,3 @@ class Borrowing(models.Model):
 
     def __str__(self):
         return f"{self.user} borrowed {self.book}"
-
-
-class Payment(models.Model):
-    class Status(models.TextChoices):
-        PENDING = "PN", _("Pending")
-        PAID = "PD", _("Paid")
-
-    class Type(models.TextChoices):
-        PAYMENT = "PM", _("Payment")
-        FINE = "FN", _("Fine")
-
-    status = models.CharField(
-        max_length=2,
-        choices=Status,
-        default=Status.PENDING
-    )
-    type = models.CharField(
-        max_length=2,
-        choices=Type
-    )
-    borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE,
-                                  related_name="payments")
-    session_url = models.URLField(max_length=500)
-    session_id = models.CharField(max_length=255, unique=True)
-    money = models.DecimalField(
-        max_digits=10,
-        decimal_places=2,
-    )
