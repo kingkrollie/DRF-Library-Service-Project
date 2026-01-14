d49e30bfc7856d9efc762d7b8c273a0e2ebefebd
Andrii Chychkan <mywork1728@gmail>
2026-01-14 14:45:11 +0200

    chore:added admin model for payment and fixed payment.urls

--- payment/urls.py
+++ payment/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
 from .views import PaymentViewSet
 
 router = DefaultRouter()
-router.register("payments", PaymentViewSet)
+router.register("payments", PaymentViewSet, basename="payments")
 
 urlpatterns = [path("", include(router.urls))]
+
+app_name = "payment"
