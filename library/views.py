f0a55b1a3f5309bddbfc57aec111f2710fedd59a
Vadym Vynokur <king.krollie@gmail.com>
2026-01-14 11:25:50 +0200

    feat: add endpoints for books with IsAdminOrIfAuthenticatedReadOnly permission class

--- library/views.py
+++ library/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from library.models import Book
+from library.serializers import BookSerializer
+from library.permissions import IsAdminOrIfAuthenticatedReadOnly
+
+
+class BookViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    GenericViewSet,
+):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+
