778c16bc30a0d606a0f199fd73b7412fd510814a
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2026-01-14 15:02:13 +0200

    feat: add tests for users application
    Added tests for:
       - Models
       - Serializers
       - Views
       - Api

---
+++ users/tests/test_models.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        """Test creating user with email"""
+        email = "test@example.com"
+        password = "password123"
+        user = get_user_model().objects.create_user(email=email, password=password)
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+        self.assertFalse(user.is_staff)
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with invalid email"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, "password123")
+
+    def test_create_superuser(self):
+        """Test create superuser"""
+        user = get_user_model().objects.create_superuser(
+            "admin@example.com", "password123"
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
---
+++ users/tests/test_serializers.py
@@ -0,0 +1,41 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from users.serializers import UserSerializer
+
+
+class SerializerTests(TestCase):
+    def test_user_serializer(self):
+        """Test user serializer validation """
+        payload = {
+            "email": "test@example.com",
+            "password": "VeryLongPassword",
+        }
+        serializer = UserSerializer(data=payload)
+
+        self.assertTrue(serializer.is_valid())
+
+    def test_user_serializer_validation(self):
+        """Test user serializer validation """
+        payload = {
+            "email": "test@example.com",
+            "password": "short",
+            "first_name": "John"
+        }
+        serializer = UserSerializer(data=payload)
+
+        self.assertFalse(serializer.is_valid())
+        self.assertIn("password", serializer.errors)
+
+
+    def test_user_serializer_update(self):
+        """Test user serializer update"""
+        user = get_user_model().objects.create_user("old@ex.com", "pass123")
+        payload = {"first_name": "NewName", "password": "newpassword123"}
+
+        serializer = UserSerializer(instance=user, data=payload, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        user.refresh_from_db()
+        self.assertEqual(user.first_name, "NewName")
+        self.assertTrue(user.check_password("newpassword123"))
