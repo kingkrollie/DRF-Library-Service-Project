f652052a66fb1d30f68b5531bc0176fea26fd177
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2026-01-14 19:02:52 +0200

    feat: add annotation for swagger documentation

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import generics
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.permissions import IsAuthenticated
@@ -7,6 +8,12 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from users.serializers import UserSerializer, AuthTokenSerializer
 
 
+@extend_schema_view(
+    post=extend_schema(
+        description="Create new account for user",
+        responses={201: UserSerializer},
+    )
+)
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
@@ -16,6 +23,17 @@ class LoginUserView(TokenObtainPairView):
     serializer_class = AuthTokenSerializer
 
 
+@extend_schema_view(
+    get=extend_schema(
+        description="Return authorized user profile data"
+    ),
+    put=extend_schema(
+        description="Update user data (full)",
+    ),
+    patch=extend_schema(
+        description="Update user data (partial)",
+    )
+)
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
     authentication_classes = (JWTAuthentication,)
