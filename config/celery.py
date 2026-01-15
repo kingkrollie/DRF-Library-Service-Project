4e9b29ba35877087d6cbfff086ba0dc9845a2322
ivan <ivanventik@gmail.com>
2026-01-15 18:32:22 +0200

    feat: addde  edis + Celery for background Telegram notifications

---
+++ config/celery.py
@@ -0,0 +1,12 @@
+import os
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+app = Celery("config")
+
+redis_host = os.getenv("REDIS_HOST", "redis")
+redis_port = os.getenv("REDIS_PORT", "6379")
+app.conf.broker_url = f"redis://{redis_host}:{redis_port}/0"
+
+app.autodiscover_tasks()
