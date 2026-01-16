db0a0be681513f8423c3897516f2904e83f1418f
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2026-01-16 16:18:22 +0200

    feat: add fee payment
    V-1 with DRY

--- library/views.py
+++ library/views.py
@@ -16,6 +16,8 @@ from library.serializers import (
     BookSerializer,
 )
 from notifications.tasks import notify_new_borrowing
+from payment.models import Payment
+from payment.services import create_payment_fee_session
 
 
 class BookViewSet(
@@ -126,6 +128,17 @@ class BorrowingReturnView(APIView):
 
         Book.objects.filter(pk=borrowing.book_id).update(
             inventory=F("inventory") + 1)
+        if borrowing.actual_return_date <= borrowing.expected_return_date:
+            session = create_payment_fee_session(borrowing)
+
+            Payment.objects.create(
+                borrowing=borrowing,
+                session_id=session.id,
+                session_url=session.url,
+                money=session.fee_amount,
+                status=Payment.Status.PENDING,
+                type=Payment.Type.FINE,
+            )
 
         return Response(BorrowingReadSerializer(borrowing).data, 
                         status=status.HTTP_200_OK)
--- payment/services.py
+++ payment/services.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 
 stripe.api_key = settings.STRIPE_API_KEY
+FINE_MULTIPLAYER = 2
 
 def create_payment_session(borrowing):
     total_price = borrowing.total_price
@@ -22,8 +23,33 @@ def create_payment_session(borrowing):
                 "quantity": 1,
             }
         ],
-        success_url="http://localhost:8000/success?session_id={CHECKOUT_SESSION_ID}",
-        cancel_url="http://localhost:8000/cancel",
+        success_url="http://127.0.0.1:8000/success?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url="http://127.0.0.1:8000/cancel",
     )
+
+    return session
+
+def create_payment_fee_session(borrowing):
+    days = (borrowing.actual_return_date - borrowing.expected_return_date).days
+    fee_price = (days * borrowing.book.daily_fee) * FINE_MULTIPLAYER
+    session = stripe.checkout.Session.create(
+        mode="payment",
+        payment_method_types=["card"],
+        line_items=[
+            {
+                "price_data": {
+                    "currency": "usd",
+                    "product_data": {
+                        "name": f"Borrowing #{borrowing.id}",
+                    },
+                    "unit_amount": int(fee_price * 100),
+                },
+                "quantity": 1,
+            }
+        ],
+        success_url="http://127.0.0.1:8000/success?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url="http://127.0.0.1:8000/cancel",
+    )
+    session.fee_amount = fee_price
 
     return session
