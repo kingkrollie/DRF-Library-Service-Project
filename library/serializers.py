b601303e56b64af2f8e4126b7f299b7da7c0e279
Vadym Vynokur <king.krollie@gmail.com>
2026-01-23 14:50:00 +0200

    fix: DRY

--- library/serializers.py
+++ library/serializers.py
@@ -8,24 +8,11 @@ from payment.services import create_payment_session
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = (
-            "id",
-            "title",
-            "author",
-            "cover",
-            "inventory",
-            "daily_fee",
-        )
-
-
-class BookDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
         fields = ("id", "title", "author", "cover", "inventory", "daily_fee")
 
 
 class BorrowingReadSerializer(serializers.ModelSerializer):
-    book = BookDetailSerializer(read_only=True)
+    book = BookSerializer(read_only=True)
 
     class Meta:
         model = Borrowing
